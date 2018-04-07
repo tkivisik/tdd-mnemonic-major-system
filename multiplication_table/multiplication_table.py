e2ac5765d8620d558d1c92578767353f8a906b48
Taavi Kivisik <taavi.kivisik@gmail.com>
2018-04-07 15:51:45 +0200

    Dimension updating works;+parametrized tests

--- multiplication_table/multiplication_table.py
+++ multiplication_table/multiplication_table.py
@@ -4,17 +4,22 @@ class NotPositiveDimensionsException(Exception):
 class TooBigDimensionsException(Exception):
     pass
 
-DIMENSION_MAX = 20
 class MultiplicationTable(object):
-    def __init__(self, height, width):
-        if height <= 0 or width <= 0:
-            raise NotPositiveDimensionsException("MultiplicationTable expects positive height and width")
-        if height > DIMENSION_MAX or width > DIMENSION_MAX:
-            raise TooBigDimensionsException("Dimensions must be <= {}".format(DIMENSION_MAX))
+    def __init__(self, height, width, DIMENSION_MAX=20):
+        self.DIMENSION_MAX = DIMENSION_MAX
         self.height = height
         self.width = width
 
+        self.verify_dimensions()
+
+    def verify_dimensions(self):
+        if self.height <= 0 or self.width <= 0:
+            raise NotPositiveDimensionsException("MultiplicationTable expects positive height and width")
+        if self.height > self.DIMENSION_MAX or self.width > self.DIMENSION_MAX:
+            raise TooBigDimensionsException("Dimensions must be <= {}".format(self.DIMENSION_MAX))
+
     def show_table_string(self):
+        self.verify_dimensions()
         table_string = ""
         for h in range(1, self.height + 1):
             line = ''
@@ -23,3 +28,11 @@ class MultiplicationTable(object):
                 line = "{}{}".format(line, new_element)
             table_string = "{}{}\n".format(table_string, line)
         return table_string
+    
+    def update_height(self, amount):
+        self.height = self.height + amount
+        self.verify_dimensions()
+
+    def update_width(self, amount):
+        self.width = self.width + amount
+        self.verify_dimensions()
--- multiplication_table/test_multiplication_table.py
+++ multiplication_table/test_multiplication_table.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 
-import multiplication_table
+import multiplication_table as mt
 from multiplication_table import NotPositiveDimensionsException, TooBigDimensionsException
 import pytest
 
 @pytest.fixture
 def table_5x5():
     '''Returns an empty MultiplicationTable'''
-    return multiplication_table.MultiplicationTable(5,5)
+    return mt.MultiplicationTable(5,5,20)
 
 def test_negative_dimensions():
     with pytest.raises(NotPositiveDimensionsException):
-        multiplication_table.MultiplicationTable(-1,3)
+        mt.MultiplicationTable(-1,3)
 
 def test_zero_zero_dimensions():
     with pytest.raises(NotPositiveDimensionsException):
-        multiplication_table.MultiplicationTable(0,0)
+        mt.MultiplicationTable(0,0)
 
 def test_too_big_dimensions():
-    ok_dimension = multiplication_table.DIMENSION_MAX
-    too_big_dimension = multiplication_table.DIMENSION_MAX+1
+    dimension_max = 2
+    ok_dimension = dimension_max
+    too_big_dimension = dimension_max + 1
     with pytest.raises(TooBigDimensionsException):
-        multiplication_table.MultiplicationTable(too_big_dimension, too_big_dimension)
-        multiplication_table.MultiplicationTable(too_big_dimension, ok_dimension)
-        multiplication_table.MultiplicationTable(ok_dimension, too_big_dimension)
+        mt.MultiplicationTable(too_big_dimension, too_big_dimension, DIMENSION_MAX=dimension_max)
+        mt.MultiplicationTable(too_big_dimension, ok_dimension, DIMENSION_MAX=dimension_max)
+        mt.MultiplicationTable(ok_dimension, too_big_dimension, DIMENSION_MAX=dimension_max)
 
 def test_correct_number_of_rows(table_5x5):
     rows = table_5x5.show_table_string().strip("\n").split("\n")
@@ -39,3 +40,46 @@ def test_first_element_not_zero(table_5x5):
     first_row = table_5x5.show_table_string().split("\n")[0]
     first_element = first_row.split()[0]
     assert int(first_element) != 0
+
+
+@pytest.mark.parametrize("height,width,added_height,added_width,expected_height,expected_width", [
+    (1, 1, 1, 0, 2, 1),
+    (1, 1, 0, 1, 1, 2)
+])
+def test_adding_to_dimensions(height,width,added_height,added_width,expected_height,expected_width):
+    table = mt.MultiplicationTable(height, width)
+    
+    if (table.DIMENSION_MAX-(height+added_height)) >= 0 and (table.DIMENSION_MAX-(width+added_width)) >= 0:
+        table.update_height(added_height)
+        table.update_width(added_width)
+        assert table.height == expected_height
+        assert table.width == expected_width
+    else:
+        with pytest.raises(TooBigDimensionsException):
+            table.update_height(added_height)
+            table.update_width(added_width)
+
+@pytest.mark.parametrize("height,width,added_height,added_width,expected_height,expected_width", [
+    (20, 20, -1, 0, 19, 20),
+    (20, 20, 0, -1, 20, 19),
+    (1, 1, -1, 0, 'error', 1),
+    (1, 1, 0, -1, 1, 'error'),
+])
+def test_subtracting_from_dimensions(height,width,added_height,added_width,expected_height,expected_width):
+    table = mt.MultiplicationTable(height, width, DIMENSION_MAX=20)
+    
+    if height+added_height >= 1 and width+added_width >= 1:
+        table.update_height(added_height)
+        table.update_width(added_width)
+        assert table.height == expected_height
+        assert table.width == expected_width
+    else:
+        with pytest.raises(NotPositiveDimensionsException):
+            table.update_height(added_height)
+            table.update_width(added_width)
+
+
+#@pytest.mark.parametrize("height,width,added_height,added_width,expected_height,expected_width", [
+#    (mt.DIMENSION_MAX, mt.DIMENSION_MAX, -1, 0, mt.DIMENSION_MAX-1, mt.DIMENSION_MAX),
+#    (mt.DIMENSION_MAX, mt.DIMENSION_MAX, 0, -1, mt.DIMENSION_MAX, mt.DIMENSION_MAX-1)
+#])
